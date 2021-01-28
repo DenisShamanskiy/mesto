b1110b41fef6292ff163ca5cf779db914ddb6b25
DenisShamanskiy <75094596+DenisShamanskiy@users.noreply.github.com>
2021-01-29 02:46:04 +0300

    Исправлены замечания 1-го ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,43 +2,32 @@ let popup = document.querySelector('.popup');
 let popupOpenButton = document.querySelector('.profile__button-edit');
 let popupCloseButton = document.querySelector('.popup__button-close');
 
-let togglePopup = function() {
-    popup.classList.toggle('popup__opened');
-}
-
-popupOpenButton.classList.add('popup__opened');
-
-popupOpenButton.addEventListener('click', togglePopup)
-popupCloseButton.addEventListener('click', togglePopup)
-
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 
-let inputName = document.querySelector('.popup__name');
-let inputJob = document.querySelector('.popup__job');
+let inputName = document.querySelector('.popup__field_type_name');
+let inputJob = document.querySelector('.popup__field_type_job');
 
 let formInput = document.querySelector('.popup__form');
 let popupSave = document.querySelector('.popup__button-save');
-let nameInput = '';
-let jobInput = '';
+
+let togglePopup = function() {
+    popup.classList.toggle('popup_opened');
+}
+
+let openPopup = function () {
+    togglePopup()
+    inputName.value = profileName.textContent
+    inputJob.value = profileJob.textContent
+}
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    nameInput = inputName.value
-    jobInput = inputJob.value
-    profileName.textContent = nameInput
-    profileJob.textContent = jobInput
+    profileName.textContent = inputName.value
+    profileJob.textContent = inputJob.value
+    togglePopup()
 }
 
+popupOpenButton.addEventListener('click', openPopup)
+popupCloseButton.addEventListener('click', togglePopup)
 formInput.addEventListener('submit', formSubmitHandler)
-popupSave.addEventListener('click', togglePopup)
-
-document.onclick = function(evt) {
-    if (evt.target.className == 'elements__like'){
-
-        evt.target.classList.add('elements__like-active');
-
-    } else {
-
-        evt.target.classList.remove('elements__like-active');}
-}
